1d0bc719256f92b8f4502196f06ee84c690e9471
Cameron S. Williamson <therealkey@outlook.com>
2023-07-08 11:04:43 -0700

    Added clap dependency and migration command (not implemented yet)

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,36 @@ mod handlers;
 mod models;
 
 use actix_web::{App, HttpServer};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates new tables in the database
+    Migrate {},
+    /// Starts the API. This is also the default mode
+    Api {},
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut cli = Cli::parse();
+
+    if let None = &cli.command {
+        cli.command = Some(Commands::Api {})
+    }
+
+    match &cli.command.unwrap() {
+        Commands::Api {} => {}
+        Commands::Migrate {} => {}
+    }
+
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
