5782728773db69812baab57d8f4be300ffcc8f80
Cameron S. Williamson <therealkey@outlook.com>
2023-07-08 22:47:02 -0700

    data dynamically displayed in a table

--- client/src/components/entries.tsx
+++ client/src/components/entries.tsx
@@ -1,41 +1,89 @@
 import { useEffect, useState } from "react";
-import GasInfo from "../utils/gasInfo";
+import { GasInfo, ConvertGasInfo, StringGasInfo } from "../utils/gasInfo";
 
 function Entries() {
-    const [entries, setEntries] = useState<Array<GasInfo>>([])
+    const [entries, setEntries] = useState<Array<GasInfo>>([]);
 
     useEffect(() => {
         fetch("http://localhost:8080/cartrax/")
-            .then((response) => response.text().then(text => {
-                let entries: Array<GasInfo> = JSON.parse(text)   
-                setEntries(entries)
-            }))
+            .then((response) =>
+                response.text().then((text) => {
+                    let entries: Array<StringGasInfo> = JSON.parse(text);
+                    setEntries(
+                        entries.map((entry) =>
+                            ConvertGasInfo.stringsToNums(entry)
+                        )
+                    );
+                })
+            )
             .catch(console.log);
-    }, [])
+    }, []);
+
+    const now = new Date();
 
     return (
         <div>
             <table>
                 <tbody>
-                {entries.map((entry, idx) => {
-                    return <tr key={idx}>
-                        
-                        <td>{entry.id}</td>
-                        <td>${entry.pricePerGallon}</td>
-                        <td>${entry.totalCost}</td>
-                        <td>{entry.gallons}</td>
-                        <td>{entry.aTripometer}</td>
-                        <td>{entry.bTripometer}</td>
-                        <td>{entry.totalTripometer}</td>
-                        <td>{entry.timeRecorded.toLocaleString()}</td>
-                        <td>{entry.city}</td>
-                        <td>{entry.state}</td>
+                    <tr>
+                        <th>Price Per Gallon</th>
+                        <th>Total Cost</th>
+                        <th>Gallons</th>
+                        <th>A Tripometer</th>
+                        <th>B Tripometer</th>
+                        <th>Total Tripometer</th>
+                        <th>Time Recorded</th>
+                        <th>City</th>
+                        <th>State</th>
                     </tr>
-                })}
+                    {entries.map((entry, idx) => (
+                        <Entry gasEntry={entry} key={idx} now={now} />
+                    ))}
                 </tbody>
             </table>
         </div>
-    )
+    );
+}
+
+function Entry(props: { gasEntry: GasInfo; now: Date }) {
+    let t = props.gasEntry.timeRecorded;
+
+    //let jan = new Date(t.getFullYear(), 0, 1);
+    //let jul = new Date(t.getFullYear(), 6, 1);
+    function isDST(d: Date) {
+        let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
+        let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
+        return Math.max(jan, jul) !== d.getTimezoneOffset();
+    }
+
+    let timeStr = `${t.getMonth() + 1}/${t.getDay()}/${t.getFullYear()}`;
+    const nowIsDst = isDST(props.now);
+    const tIsDst = isDST(t);
+
+    console.log(`now: ${nowIsDst}, then: ${tIsDst}`);
+
+    let hours = t.getHours();
+    if (nowIsDst && !tIsDst) {
+        hours++
+    } else if (tIsDst && !nowIsDst) {
+        hours--
+    }
+
+    timeStr = `${timeStr} ${hours}:${t.getMinutes()}`
+
+    return (
+        <tr>
+            <td>${props.gasEntry.pricePerGallon.toFixed(3)}</td>
+            <td>${props.gasEntry.totalCost.toFixed(2)}</td>
+            <td>{props.gasEntry.gallons.toFixed(3)}</td>
+            <td>{props.gasEntry.aTripometer.toFixed(1)}</td>
+            <td>{props.gasEntry.bTripometer.toFixed(1)}</td>
+            <td>{props.gasEntry.totalTripometer}</td>
+            <td>{timeStr}</td>
+            <td>{props.gasEntry.city}</td>
+            <td>{props.gasEntry.state}</td>
+        </tr>
+    );
 }
 
 export default Entries;
--- client/src/utils/gasInfo.ts
+++ client/src/utils/gasInfo.ts
@@ -1,12 +1,42 @@
-export default interface GasInfo {
+export interface GasInfo {
     id: number | undefined | null;
     pricePerGallon: number;
     totalCost: number;
     gallons: number;
     aTripometer: number;
     bTripometer: number;
-    totalTripometer: number
+    totalTripometer: number;
     timeRecorded: Date;
     city: string;
     state: string;
+}
+
+export interface StringGasInfo {
+    id: number | undefined | null;
+    pricePerGallon: string;
+    totalCost: string;
+    gallons: string;
+    aTripometer: string;
+    bTripometer: string;
+    totalTripometer: string;
+    timeRecorded: string;
+    city: string;
+    state: string;
+}
+
+export class ConvertGasInfo {
+    public static stringsToNums(unParsed: StringGasInfo): GasInfo {
+        return {
+            id: unParsed.id,
+            pricePerGallon: parseFloat(unParsed.pricePerGallon),
+            totalCost: parseFloat(unParsed.totalCost),
+            gallons: parseFloat(unParsed.gallons),
+            aTripometer: parseFloat(unParsed.aTripometer),
+            bTripometer: parseFloat(unParsed.bTripometer),
+            totalTripometer: parseFloat(unParsed.totalTripometer),
+            timeRecorded: new Date(unParsed.timeRecorded),
+            city: unParsed.city,
+            state: unParsed.state,
+        };
+    }
 }
