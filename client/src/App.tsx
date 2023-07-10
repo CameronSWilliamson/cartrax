73332322ccaab4d477c0b8222ed11afda2680301
Cameron S. Williamson <therealkey@outlook.com>
2023-07-09 20:07:22 -0700

    Added inputs on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Entries from "./components/entries";
+import GasInfoInput from "./components/input";
 import Stats from "./components/stats";
 
 
 function App() {
     return (
         <>
+            <GasInfoInput />
             <Stats />
             <Entries />
         </>
